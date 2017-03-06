613e790b9e6ad4324a49bf23ef951e81fc074fde
Liliana Fiore <liliana.fiorela@gmail.com>
2017-03-06 16:51:10 -0500

    subiendo botones

--- js/main.js
+++ js/main.js
@@ -18,9 +18,51 @@ window.onclick = function(event) {
 
 var mostarOcultar = function(divtexte) {
 	var divtext = document.getElementById(divtexte);
-	if (divtext.style.display == "block" ) {
-		divtext.style.display = "none";
-	} else{
-		divtext.style.display = "block";
+  var text1 = document.getElementById("text1");
+  var text2 = document.getElementById("text2");
+  var text3 = document.getElementById("text3");
+
+	if (divtext.id == "text1"  ) {
+    divtext.style.display ="block";
+		text3.style.display = "none";
+    text2.style.display ="none";
 	}
+  if (divtext.id == "text2"  ) {
+    divtext.style.display ="block";
+    text1.style.display = "none";
+    text3.style.display ="none";
+  }
+  if (divtext.id == "text3"  ) {
+    divtext.style.display ="block";
+    text1.style.display = "none";
+    text2.style.display ="none";
+  }
+}
+
+document.getElementById("bt1").onclick = function(){
+  var cb = 0;  //cuenta buenas
+  var cm= 0; // cuenta malas
+  var op1 = document.getElementsByName("p1")[0].checked;
+  var op2 = document.getElementsByName("p2")[2].checked;
+  var op3 = document.getElementsByName("p3")[2].checked;
+
+  op1 ? cb++ : cm++;
+  op2 ? cb++ : cm++;
+  op3 ? cb++ : cm++;
+
+ document.getElementById("quiz1").innerHTML ="Tienes " +cb + " correcta(s)";
+}
+
+document.getElementById("bt2").onclick = function(){
+  var cb = 0;  //cuenta buenas
+  var cm= 0; // cuenta malas
+  var op1 = document.getElementsByName("p4")[1].checked;
+  var op2 = document.getElementsByName("p5")[0].checked;
+  var op3 = document.getElementsByName("p6")[1].checked;
+
+  op1 ? cb++ : cm++;
+  op2 ? cb++ : cm++;
+  op3 ? cb++ : cm++;
+
+ document.getElementById("quiz2").innerHTML ="Tienes " +cb + " correcta(s)";
 }
